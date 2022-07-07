22f132846b496dbb78f01716d5379b77c20a3135
Sergey <graygmai1@gmail.com>
2022-07-07 21:29:39 +0500

    добавление обектов к модулям

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,29 @@
-import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
-import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-import './app.css';
+import "./app.css";
 
 function App() {
+  const data = [
+    { name: "Sergey", salary: "3000", increase: true },
+    { name: "Pasha", salary: "2000" },
+    { name: "Sasha", salary: "4000" },
+  ];
+
   return (
     <div className="app">
-        <AppInfo />
+      <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
     </div>
   );
 }
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,15 +1,22 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-import './employees-list.css';
+import "./employees-list.css";
 
-const EmployeesList = () => {
+const EmployeesList = ({ data }) => {
+
+    const elements = data.map((item) => {
+
+        return (
+            <EmployeesListItem {...item} />
+        )
+
+    });
+
     return (
-        <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+        <ul className='app-list list-group'>
+            {elements}
         </ul>
     )
-}
+};
 
 export default EmployeesList;
