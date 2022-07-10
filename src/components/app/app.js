fa3f2be4ead7bfb2f9088402c96b1fa22ed5dd84
Sergey <graygmai1@gmail.com>
2022-07-10 07:31:16 +0500

    search panel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,110 @@
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
-
 import "./app.css";
 
-function App() {
-  const data = [
-    { name: "Sergey", salary: "3000", increase: true },
-    { name: "Pasha", salary: "2000" },
-    { name: "Sasha", salary: "4000" },
-  ];
+class App extends Component {
 
-  return (
-    <div className="app">
-      <AppInfo />
+	constructor() {
+		super()
+		this.state = {
+			data: [
+				{ name: "Sergey", salary: "3000", id: 1, increase: false, like: true },
+				{ name: "Pasha", salary: "2000", id: 2, increase: true, like: false },
+				{ name: "Sasha", salary: "4000", id: 3, increase: false, like: false },
+			]
+		}
+		this.maxId = 4;
+	}
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
-      </div>
+	onDelete = (id) => {
+		this.setState(({ data }) => {
+			return {
+				data: data.filter(item => item.id !== id)
+			}
+		})
+	}
 
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+	onAdd = (name, salary) => {
+
+		const newItem = {
+			name,
+			salary,
+			increase: false,
+			like: false,
+			id: this.maxId++
+		}
+
+		this.setState(({ data }) => {
+			const newArr = [...data, newItem]
+			return {
+				data: newArr
+			}
+		})
+
+	}
+	onIncrase = (id) => {
+
+		this.setState(({ data }) => ({
+
+			data: data.map(item => {
+
+				if (item.id === id) {
+					return { ...item, increase: !item.increase }
+				}
+				return item
+			})
+
+		}))
+	}
+	onLike = (id) => {
+
+		this.setState(({ data }) => ({
+
+			data: data.map(item => {
+
+				if (item.id === id) {
+					return { ...item, like: !item.like }
+				}
+				return item
+			})
+
+		}))
+	}
+
+	render() {
+		const { data } = this.state
+
+		return (
+			<div className="app">
+				<AppInfo data={data} />
+
+				<div className="search-panel">
+					<SearchPanel data={data} />
+					<AppFilter />
+				</div>
+
+				<EmployeesList
+					data={data}
+					onIncrase={this.onIncrase}
+					onLike={this.onLike}
+					onDelete={this.onDelete}
+				/>
+
+				<EmployeesAddForm onAdd={this.onAdd} />
+
+			</div>
+		)
+	}
+
+
+
 }
 
 export default App;
+
+
